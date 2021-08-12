f9aec582223b067b791a21d8fa5f574e98fe1d76
栾天 <tian.luan@percent.cn>
2021-08-12 19:16:55 +0800

    完成第五周 3-6

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,23 +1,149 @@
 'use strict';
 
+const fs = require('fs')
+const fse = require('fs-extra')
+const inquirer = require('inquirer')
+const semver = require('semver')
+
+
 const log = require('@icream-cli/log')
 const Command = require('@icream-cli/command')
 
-function init(argv) {
-  return new InitCommand(argv)
+const TYPE_PROJECT = 'project'
+const TYPE_COMPONENT = 'component'
+
+let prompt = {}
+prompt.projectName = {
+  type: 'input',
+  name: 'projectName',
+  message: '请输入项目名称',
+  default: '',
+  validate: function(v) {
+    const done = this.async()
+    setTimeout(() => {
+      if (!/^[a-zA-Z]+[\w-]*[a-zA-Z0-9]$/.test(v)) {
+        done('输入的项目名称不合法')
+        return
+      }
+      done(null, true)
+    }, 0)
+  },
+  filter: (v) => v
+}
+
+prompt.version = {
+  type: 'input',
+  name: 'version',
+  message: '请输入项目版本号',
+  default: '0.0.1',
+  validate: function(v) {
+    const done = this.async()
+    setTimeout(() => {
+      if (!semver.valid(v)) {
+        done('输入的项目名称不合法')
+        return
+      }
+      done(null, true)
+    }, 0)
+  },
+  filter: (v) => semver.valid(v) || v
 }
 
 class InitCommand extends Command {
   init() {
     this.projectName = this._argv[0] || ''
     this.opts = this._argv[this._argv.length - 1]
+    this.force = this.opts.force
     log.verbose('projectName', this.projectName)
-    log.verbose('force', this.opts.force)
+    log.verbose('force', this.force)
   }
 
-  exec() {
+  async exec() {
+    try {
+      // 准备阶段
+      // 下载模板
+      // 安装模板
+      await this.prepare()
+    } catch (error) {
+      log.error(error.message)
+    }
+  }
+
+  async prepare() {
+    // process.cwd() 当前执行命令的路径
+    const localPath = process.cwd()
+
+    if (!this.force && !this.isDirEmpty(localPath)) {
+      return log.error('当前目录不为空，请清空目录或者--force强制创建项目')
+    }
+
+    if (!this.isDirEmpty(localPath) && this.force) {
+      const { ifContinue } = await inquirer.prompt(
+        {
+          type: 'confirm',
+          name: 'ifContinue',
+          default: false,
+          message: '当前文件夹不为空，是否继续创建项目？'
+        }
+      )
+      if (ifContinue) {
+        // 启动强制更新  清空当前目录
+        console.log('localPath', localPath)
+        // fse.emptyDirSync(localPath)
+        console.log('此处执行清空代码，暂时注释')
+      }
+    }
+
+    return this.getProjectInfo()
 
   }
+
+  async getProjectInfo() {
+    // 选择创建项目或者是组件
+    const { type } = await inquirer.prompt({
+      type: 'list',
+      message: '请选择初始化类型',
+      name: 'type',
+      default: TYPE_PROJECT,
+      choices: [
+        {
+          name: '项目',
+          value: TYPE_PROJECT
+        },
+        {
+          name: '组件',
+          value: TYPE_COMPONENT
+        },
+      ]
+    })
+
+    log.verbose('type', type)
+
+    if (type === TYPE_PROJECT) {
+      const o = inquirer.prompt([
+        prompt.projectName,
+        prompt.version
+      ])
+      // console.log('o', o)
+    } else if (type === TYPE_COMPONENT) {
+
+    }
+
+  }
+
+  isDirEmpty(localPath) {
+    let fileList = fs.readdirSync(localPath)
+    fileList = fileList.filter(file => (
+      !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
+    ))
+    return !fileList || fileList.length <= 0
+  }
+
+}
+
+
+function init(argv) {
+  return new InitCommand(argv)
 }
 
 module.exports = init;
